9f00ed98d586bc8ee1a1bdd3385ff5b778fa909c
Stevanzzo <steve.anzzo@gmail.com>
2021-01-09 20:56:23 -0600

    added premake

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+project "IMGUI"
+	kind "StaticLib"
+	language "C++"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+		staticruntime "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter {"system:windows", "configurations:Release"}
+		buildoptions "/MT"
